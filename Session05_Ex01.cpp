659718500862f126b3f9477f8e8ce09080e48484
Ngô Trung Chiến <ngotrungchien232@gmail.com>
2024-11-25 15:52:42 +0700

    C_Session05_Ex01

---
+++ Session05_Ex01.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+	
+	//khai bao bien 
+	int i;
+	
+	//vong lap for
+	for(i = 100; i > 0; i--){
+		printf("%d\n", i);
+	}
+	
+	//ket thuc chuong trinh
+	return 0;
+	
+}
